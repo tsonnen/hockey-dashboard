9d6fb9a0fe4572e2113f529a2b546204538e545e
tsonnen <tsonnen67@gmail.com>
2025-10-20 08:57:09 -0700

    More correct bypass for E2E testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL ?? 'http://localhost:3000/',
-
+    extraHTTPHeaders: {
+      'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET ?? '',
+      'x-vercel-set-bypass-cookie': 'samesitenone',
+    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
